84cc886b9447fc8c8f719fa437303735109cd9bc
harshit <harshitgpt07@gmail.com>
2021-10-19 21:26:09 +0530

    initial commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {
+mongoose.connect("mongodb+srv://admin-harshit:Test123@cluster0.snews.mongodb.net/todolistDB", {
   useNewUrlParser: true
 });
 
